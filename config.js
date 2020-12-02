ae9b96eeec2a4e39a1ae58c43112a22021bd0c79
toshani08 <60036420+toshani08@users.noreply.github.com>
2020-12-02 17:53:25 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA96ApYhgQ2wyaCU-yil2x-EYrc7UYOrvE",
+    authDomain: "booksanta-a56ac.firebaseapp.com",
+    databaseURL: "https://booksanta-a56ac.firebaseio.com",
+    projectId: "booksanta-a56ac",
+    storageBucket: "booksanta-a56ac.appspot.com",
+    messagingSenderId: "401700544738",
+    appId: "1:401700544738:web:f8643548d208860c737ebb"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
